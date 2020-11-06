d8bb1a0b2e47426920ce4391afa5c736b9286c6b
siddhantagarkar <siddhantagarkar35@gmail.com>
2020-11-06 10:50:43 +0530

    FirstExample is Commited

---
+++ secondexample.java
@@ -0,0 +1,8 @@
+public class secondexample {
+    public static void printEvenNumber(int no) {
+        System.out.println("Even number upto"+no);
+        for (int x=0;x < no; x = x+2){
+            System.out.println(x);
+        }
+    }
+}
---
+++ thirdexample.java
@@ -0,0 +1,9 @@
+public class thirdexample {
+    public static void printOddnNumber(int no) {
+        System.out.println("Odd number upto"+no);
+        for (int x=0;x < no; x = x+2){
+            System.out.println(x);
+        }
+    }
+}
+
