09d9e0fabcd0c5a0e4dbcac58d63c28faaed60a6
siddhantagarkar <siddhantagarkar35@gmail.com>
2020-11-06 11:54:58 +0530

    AddefFeatures

--- firstexample.java
+++ firstexample.java
@@ -3,6 +3,10 @@ public class firstexample
     public static void main(String[] args) {
 
         System.out.println("Hello there");
+        System.out.println("calling the even numbr func");
+        secondexample.printEvenNumber(11);
+        System.out.println("calling the odd numbr func");
+        thirdexample.printOddnNumber(11);
     }
 
 }
