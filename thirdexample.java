bf6a77e63e349ec26cbbf1fb6126532841c1d31d
siddhantagarkar <siddhantagarkar35@gmail.com>
2020-11-06 10:58:14 +0530

    MultipleFile is Commited

--- thirdexample.java
+++ thirdexample.java
@@ -1,7 +1,7 @@
 public class thirdexample {
-    public static void printOddnNumber(int no) {
-        System.out.println("Odd number upto"+no);
-        for (int x=0;x < no; x = x+2){
+    public static void printOddnNumber(int i) {
+        System.out.println("Odd number upto"+i);
+        for (int x=1;x < i; x = x+2){
             System.out.println(x);
         }
     }
